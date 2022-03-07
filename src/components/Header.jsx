041d215cd5d599b10b5916000af318248f124118
Ivan Fedak <ivanfedak@Ivans-MacBook-Air.local>
2022-03-07 18:49:17 +0200

    headerReady

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 
+import {useState,useEffect} from 'react';
 import styled from 'styled-components';
 import Container from './Container';
 import {IoMoon,IoMoonOutline} from 'react-icons/io5'
@@ -6,13 +7,22 @@ import {IoMoon,IoMoonOutline} from 'react-icons/io5'
 
 const Header = (props) => {
 
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
+    useEffect(()=>{
+        document.body.setAttribute('data-theme', theme);
+    },[theme])
+
     return(
         <HeaderEl>
             <Container>
                 <Wrapper>
                     <Title>Where in the world?</Title>
-                    <ModeSwitcher>
-                        <IoMoon/> Light Theme
+                    <ModeSwitcher onClick={toggleTheme}>
+                        {theme === 'light' ? <IoMoonOutline/> : <IoMoon/> }
+                        <span>{theme} Theme</span>
                     </ModeSwitcher>
                 </Wrapper>
             </Container>
@@ -20,15 +30,39 @@ const Header = (props) => {
     )
 }
 
-
 export default Header;
+
 
 const HeaderEl = styled.header`
     box-shadow: var(--box-shadow);
     background-color: var(--ui-base);
 `;
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px 0;
+    font-size: 22px;
+    @media (max-width: 767.98px){
+        font-size: 18px
+    }
+    @media (max-width: 479.98px){
+        font-size: 15px
+    }
+`;
 const Title = styled.a.attrs({
     href: '/'
-})``;
-const ModeSwitcher = styled.div``;
+})`
+    font-weight: var(--fw-bold);
+`;
+const ModeSwitcher = styled.div`
+    display: flex;
+    align-items: center;
+    color: var(--main-color);
+    cursor: pointer;
+    font-weight: var(--fw-bold);
+    text-transform: capitalize;
+    span{
+        margin-left: 0.75rem
+    }
+`;
