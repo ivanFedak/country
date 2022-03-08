c55452e1e27946affea097293f626a60971ceb83
Ivan Fedak <ivanfedak@Ivans-MacBook-Air.local>
2022-03-08 09:12:11 +0200

    selectReady

--- src/components/Form/Controls.jsx
+++ src/components/Form/Controls.jsx
@@ -1,15 +1,42 @@
 import {useState,useEffect} from "react";
+import styled from 'styled-components';
 import Search from "./Search";
+import CustomSelect from "./CustomSelect";
+
+const options = [
+    {value:'Africa',label:'Africa'},
+    {value:'America',label:'America'},
+    {value:'Asia',label:'Asia'},
+    {value:'Europe',label:'Europe'},
+    {value:'Oceania',label:'Oceania'},
+]
 
 const Controls = () => {
 
     const [search,setSearch] = useState('');
+    const [region,setRegion] = useState('');
 
     return (
-        <div>
+        <Wrapper>
             <Search search={search} setSearch={setSearch}/>
-        </div>
+            <CustomSelect 
+                    options={options} placeholder="Filter by Region" 
+                    isClearable isSearchable={false} 
+                    value={region} onChange={setRegion}
+            />
+        </Wrapper>
     )
 };
 
 export default Controls;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    @media (min-width: 767.98px){//more
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+`;
--- src/components/Form/CustomSelect.jsx
+++ src/components/Form/CustomSelect.jsx
@@ -1,11 +1,32 @@
+import styled from 'styled-components';
+import Select from 'react-select';
 
-
-const CustomSelect = () => {
-    return (
-        <div>
-            CustomSelect
-        </div>
-    )
-};
+const CustomSelect = styled(Select).attrs({
+    styles: {
+        control: (provided) => ({
+            ...provided, //def styles 
+            backgroundColor: 'var(--ui-base)',
+            borderRadius: 'var(--border-radius)',
+            padding: '0.25rem',
+            border: 'none',
+            boxShadow: 'var(--box-shadow)',
+            height: '50px'
+        }),
+        option: (provided,state) => ({
+            ...provided,
+            cursor: 'pointer',
+            color:  'var(--main-color)',
+            backgroundColor: state.isSelected ? 'var(--body-bg)' : 'var(--ui-base)',
+        })
+    },
+})`
+    width: 200px;
+    border-radius: var(--border-radius);
+    font-family: var(--family);
+    & > * {box-shadow: var(--box-shadow)}
+    & * {color: var(--main-color) !important}
+    & input {padding-left: 0.25rem}
+    & > div[id] {background-color: var(--ui-base)}
+`;
 
 export default CustomSelect;
